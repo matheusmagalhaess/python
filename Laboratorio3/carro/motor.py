fe3bc606edd0558603083ff2318ff2b102b49322
Matheus Magalhães <mmagalhaes67@gmail.com>
2020-09-15 18:30:36 -0300

    update

---
+++ Laboratorio3/carro/motor.py
@@ -0,0 +1,10 @@
+def nomeModulo():
+    return "Módulo motor.py"
+    
+
+caracteristicas_motor = {
+    "fabricante": "Mercedes",
+    "combustivel": "Flex",
+    "numValvulas": "8",
+    "potencia": "500 cv"
+}
---
+++ Laboratorio3/mymod.py
@@ -0,0 +1,14 @@
+# Modulo Laboratorio 3
+# Matheus Magalhaes 
+
+def comprimentoString(str):
+    '''Retorna o tamanho da string'''
+    tamanho = len(str)
+    return tamanho
+    
+
+def imprimeTipo(var):
+    '''Retorna o tipo da variável'''
+    tipo = type(var)
+    return tipo
+
